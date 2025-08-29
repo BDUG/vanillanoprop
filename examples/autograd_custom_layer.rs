ee5e77a6e5765988f383eefd1e4d5ca2f1135a9e
BDUG <dirk.bangel@gmx.de>
2025-08-29 18:52:32 +0200

    Add basic autograd node and example

---
+++ examples/autograd_custom_layer.rs
@@ -0,0 +1,27 @@
+use vanillanoprop::tensor::{Node, NodeRef, Tensor};
+
+struct MyLayer {
+    weight: NodeRef,
+}
+
+impl MyLayer {
+    fn new() -> Self {
+        // Single weight parameter initialised to 2.0
+        let w = Tensor::new(vec![2.0], vec![1, 1]).into_node(true);
+        Self { weight: w }
+    }
+
+    fn forward(&self, x: &NodeRef) -> NodeRef {
+        // Simple linear layer y = x * w
+        Node::matmul(x, &self.weight)
+    }
+}
+
+fn main() {
+    let layer = MyLayer::new();
+    let x = Tensor::new(vec![3.0], vec![1, 1]).into_node(true);
+    let y = layer.forward(&x);
+    Node::backward(&y);
+    println!("grad x: {:?}", x.borrow().grad.as_ref().unwrap().data);
+    println!("grad w: {:?}", layer.weight.borrow().grad.as_ref().unwrap().data);
+}
